c270b31e8e7ad00aed5f3f5a83d3d7be884b1d4e
root <jatin.soni007@gmail.com>
2021-10-08 09:10:29 +0530

    deploy 1

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import connectDB from './config/db.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
@@ -13,10 +14,6 @@ connectDB();
 
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send(' Api is running...');
-});
-
 app.use('/api/products',productRouter);
 app.use('/api/users',userRouter);
 app.use('/api/orders',orderRouter);
@@ -24,6 +21,22 @@ app.use('/api/orders',orderRouter);
 app.use('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 });
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....lululu')
+    })
+}
+
 
 app.use((err,req,res,next)=>{
     const statusCode = res.statusCode == 200 ? 599 : res.statusCode;
@@ -34,5 +47,5 @@ app.use((err,req,res,next)=>{
     })
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 18000;
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on ${PORT}....`));
